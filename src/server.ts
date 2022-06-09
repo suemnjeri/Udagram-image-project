fc2337fa0e41f88781613bc3b299020500cbae27
suemnjeri <suemnjeri@gmail.com>
2022-06-09 17:46:13 +0300

    updated collection, added typescript

--- src/server.ts
+++ src/server.ts
@@ -33,7 +33,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   //! END @TODO1
 
-  app.get("/filteredimage/", async (req, res) => {
+  app.get("/filteredimage/", async (req:express.Request, res:express.Response) => {
     let {image_url} = req.query;
     if (!image_url){
       return res.status(400).send('An image URL is required');
@@ -52,7 +52,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req:express.Request, res:express.Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
